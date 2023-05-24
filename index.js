0cca577e41e0450d3727ca5519042271aa212984
Orlando Murcio <ato.murcio@gmail.com>
2023-05-24 16:48:29 -0600

    selectAll().style()

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var s = d3.select('div').text();
+// select first elements text
+// var s = d3.select('div').text();
 
+// select all divs and change font color
+var s = d3.selectAll('div').style("color", "orange")
 
 console.log(s)
