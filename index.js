27d9b1adaba1bf2668ae9d2b1c4ddcfc05a27b26
Orlando Murcio <ato.murcio@gmail.com>
2023-05-24 20:06:44 -0600

    datum() enter() data()

--- index.js
+++ index.js
@@ -29,3 +29,47 @@
 //   .style("background-color", "purple")
 //   .attr("class", "some random class");
 
+// datum() appends the whole data set to every node element in the dom, we duplicate the data for each node entry
+// d3.select('body')
+//   .selectAll('p')
+//   .datum(['1', '2'])
+//   .text(d => d);
+//data() associates each data point for each node element
+// if we have more data than node element the data just doesnt show in this case
+// d3.select('body')
+//   .selectAll('p')
+//   .data([1,2,3])
+//   .text(d => `- ${d}`)
+
+// d3.select('body')
+//   .selectAll('p')
+//   .data([1,2,3,4,5])
+//   .text(d => d)// we have to assing the text for the existing node elements
+//   .enter() //bind the data
+//   .append('p')// we are appending p tag for the missing node elements, we currently only have
+//   .text(d => d) //add text to the newly created p tags
+
+// var myData=["London","New york","Paris"]
+
+// d3.select('body')
+//   .selectAll('h1')
+//   .data(myData)
+//   .enter()
+//   .append('h1')
+//   .text(d => d)
+
+  /* here is another variation for the same problem 
+  var mydata=["London","New york","Paris"]
+d3.select("body") // Creating three h1 tags using append function.
+  .append("h1")
+d3.select("body")
+  .append("h1")
+d3.select("body")
+  .append("h1")    
+d3.select("body") // Binding data mydata elements with h1 tags
+  .selectAll("h1")
+  .data(mydata)
+  .text(function(d) { return d;}) 
+  */ 
+
+  
